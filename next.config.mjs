2c394ed413d8cb87af5f9a8ec2033dae62535f6b
Schumynet <netprime311@gmail.com>
2025-05-10 00:06:58 +0200

    Update next.config.mjs
    Signed-off-by: Schumynet <netprime311@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.mjs");
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
+  // Configurazione per GitHub Pages
+  output: "export",
+  basePath: "/netprimestreaming", // Usa il nome esatto del repository GitHub
+  assetPrefix: "/netprimestreaming/",
+
+  // Impostazioni aggiuntive
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
@@ -27,7 +23,6 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  swcMinify: true,
 };
 
 export default config;
